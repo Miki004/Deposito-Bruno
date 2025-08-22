eeaa99e901f71fefabeee983ce35b93132194899
Miki004 <162804895+Miki004@users.noreply.github.com>
2025-08-22 15:34:20 +0200

    chore: inserita una piccola chat history

--- 22-08/counter.py
+++ 22-08/counter.py
@@ -8,7 +8,26 @@ if 'messages' not in st.session_state:
 
 st.title("Simple Chat")
 
-st.session_state.name = st.text_input("Inserisci un messaggio:")
-response = ask(st.session_state.name)
-st.write(response.choices[0].message.content)
+user_input = st.text_input("Inserisci un messaggio:")
 
+if st.button("Send"):
+    #salva il messaggio dell'utente
+    st.session_state.messages.append({"role": "user", "content": user_input})
+    try:
+        response = ask(user_input)
+        assistant_response = response.choices[0].message.content
+
+        #mostro a video la risposta
+        st.write(f"{assistant_response}")
+
+        #salvo il messaggio del modello
+        st.session_state.messages.append({"role":"assistant", "content":assistant_response})
+    except Exception as e :
+        st.error(f"{e}")
+
+
+
+if st.button("Show History Chat"):
+    for messages in st.session_state.messages:
+        if messages["role"] == "user":
+            st.write(messages["content"])
